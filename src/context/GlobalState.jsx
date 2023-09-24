0d6fb4e44a6955377b3407fb5ac0067f78a4a9ec
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-09-23 19:34:01 -0600

    modificando el initState

--- src/context/GlobalState.jsx
+++ src/context/GlobalState.jsx
@@ -1,11 +1,25 @@
 /* eslint-disable react/prop-types */
-import { createContext  } from "react";
+import { createContext, useContext, useReducer  } from "react";
+import AppReducer from "./AppReducer";
 
-export const Context = createContext()
+const Context = createContext()
+
+export const useGlobalState = () => {
+  const context = useContext(Context)
+  return context
+}
+const initalState = {
+  transactions: []
+}
 
 const GlobalProvider = ({children}) => {
+  const [state, dispatch] = useReducer(AppReducer, initalState)
+  
   return (
-    <Context.Provider value={{ total: 100 }}>
+    <Context.Provider value={{ 
+      transactions: state.transactions,
+      dispatch
+     }}>
       {children}
     </Context.Provider>
   )
