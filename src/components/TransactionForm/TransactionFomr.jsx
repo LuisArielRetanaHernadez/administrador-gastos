4810894b779c8126fefdf2e33b85d214ce12d682
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-09-23 20:47:14 -0600

    usando la funcion addTransaction

--- src/components/TransactionForm/TransactionFomr.jsx
+++ src/components/TransactionForm/TransactionFomr.jsx
@@ -1,7 +1,32 @@
+import { useState } from "react"
+
+import { useGlobalState } from "../../context/GlobalState"
+
 const TransactionForm = () => {
+  const [description, setDescription] = useState('')
+  const [amount, setAmount] = useState(0)
+
+  const {addTransaction} = useGlobalState()
+
+  const onSubmit = e => {
+    e.preventDefault()
+
+    addTransaction({
+      description,
+      amount
+    })
+  }
   return (
     <div>
-      TransactionForm
+      <form onSubmit={onSubmit}>
+        <input type="text" placeholder="Enter a Description"
+        onChange={(e) => setDescription(e.target.value)}
+         />
+        <input type="number" step={0.01} placeholder="00.00"
+        onChange={(e) => setAmount(e.target.value)}
+         />
+        <button>Add Transacion</button>
+      </form>
     </div>
   )
 }
