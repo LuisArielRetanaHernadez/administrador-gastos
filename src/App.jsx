6ba6b38ee330ec5d5c698c9549b409b3c733e049
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-10-02 17:27:52 -0600

    agregando el componente Balance, TransactionForm y TransactionList

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import './App.css'
+import Balance from './components/Balance/Balance'
 import Header from './components/Header/Header'
+import TransactionForm from './components/TransactionForm/TransactionFomr'
+import TransactionList from './components/TransactionForm/TransactionLista'
 import GlobalProvider from './context/GlobalState'
 function App() {
 
@@ -7,6 +10,9 @@ function App() {
     <>
     <GlobalProvider>
       <Header />
+      <Balance />
+      <TransactionForm />
+      <TransactionList />
       <h1>Hello Word</h1>
     </GlobalProvider>
     </>
