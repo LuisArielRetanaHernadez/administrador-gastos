2a15b3965eec66572f2012e30b947911adf93f31
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-10-02 18:22:17 -0600

    cambiando la importacion del transaction y haciendo un map a los amount y por ultimo haciendo un reducer para llegar al total de los amounts

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -1,7 +1,15 @@
+import { useGlobalState } from "../../context/GlobalState"
+
 const Balance = () => {
+  const { transactions } = useGlobalState()
+  
+  const amounts = transactions.map(transaction => transaction.amount)
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2)
+
   return (
     <div>
-      Balance
+      <h3>Balance</h3>
+      {total}
     </div>
   )
 }
