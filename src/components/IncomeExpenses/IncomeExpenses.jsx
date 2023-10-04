29ca5f5e3575008bd7cd6d457498064f25ef5119
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-10-03 19:28:06 -0600

    agregando estilos git push

--- src/components/IncomeExpenses/IncomeExpenses.jsx
+++ src/components/IncomeExpenses/IncomeExpenses.jsx
@@ -8,13 +8,13 @@ const IncomeExpenses = () => {
   const expenses = (amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) * -1).toFixed(2);
   
   return (
-    <div>
-      <div>
+    <div className="">
+      <div className="flex justify-between my-2">
         <h4>Income</h4>
         <p>{income}</p>
       </div>
 
-      <div>
+      <div className="flex justify-between my-2">
         <h4>Expenses</h4>
         <p>{expenses}</p>
       </div>
